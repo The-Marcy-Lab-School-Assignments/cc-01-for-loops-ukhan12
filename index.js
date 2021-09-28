a6996932e937065e0f15ea7680888dca4aaeb7fc
uzma <ubkhan3@gmail.com>
2021-09-28 14:46:07 +0000

    Completed functions 1-4. Bonus is incomplete.

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
-//1
 function countToFifty(){
-  
+  for(let i=1; i<=50; i++){
+      console.log(i);
+  }
 }
 
 //2
-function countFromOne(){
-  
+function countFromOne(num){
+  for(let i=1; i<=num; i++){
+      console.log(i);
+  }
 }
 
 //3
 function threeAndFiveInFifty(){
-  
+  for(let i=0; i<=50; i++){
+       if(i % 3 === 0 || i % 5 === 0){
+          console.log(i);
+      }
+  } 
 }
 
 //4
-function threeAndFive(){
-  
+function threeAndFive(num){
+  for(let i=0; i<=num; i++){
+       if(i % 3 === 0 || i % 5 === 0){
+          console.log(i);
+      }
+  } 
 }
 
+// //bonus
+// function triple(...arr){
+//   let newArr = [];
+//   arr.forEach(i =>{ newArr[i]=arr[i]*3});
+//   return newArr;
+// }
 
+countToFifty();
+countFromOne(5);
+threeAndFiveInFifty();
+threeAndFive(10);
+//console.log(triple(1,2,3,4));
